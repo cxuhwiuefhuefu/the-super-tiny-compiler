d8bef9ba833ac5e0ac1c40088bc8f5a3764485ec
Sunny <Sunny_chen@fionacos.com>
2023-02-28 18:35:37 +0800

    token 词法分析

---
+++ tokenizer.spec.ts
@@ -0,0 +1,65 @@
+import {test, expect} from 'vitest'
+import {tokenizer, TokenTypes} from './tokenizer'
+
+test.skip('tokenizer', () => {
+  const code = `(add 2 (subtract 4 2))`
+  const tokens = [
+    { type: TokenTypes.Paren, value: '(' },
+    { type: TokenTypes.Name, value: 'add' },
+    { type: TokenTypes.Number, value: '2' },
+    { type: TokenTypes.Paren, value: '(' },
+    { type: TokenTypes.Name, value: 'subtract' },
+    { type: TokenTypes.Number, value: '4' },
+    { type: TokenTypes.Number, value: '2' },
+    { type: TokenTypes.Paren, value: ')' },
+    { type: TokenTypes.Paren, value: ')' },
+  ]
+
+  expect(tokenizer(code).toEqual(tokens))
+})
+
+
+test.skip('paren', () => {
+  const code = `(`
+  const tokens = [{ type: TokenTypes.Paren, value: "(" }];
+
+  expect(tokenizer(code).toEqual(tokens))
+})
+
+test.skip('right paren', () => {
+  const code = `(`
+  const tokens = [{ type: TokenTypes.Paren, value: "(" }];
+
+  expect(tokenizer(code).toEqual(tokens))
+})
+
+
+
+test.skip('add', () => {
+  const code = `add`
+  const tokens = [{ type: TokenTypes.Name, value: "add" }];
+
+  expect(tokenizer(code).toEqual(tokens))
+})
+
+
+test.skip('number', () => {
+  const code = `22`
+  const tokens = [{ type: TokenTypes.Number, value: "a" }];
+
+  expect(tokenizer(code).toEqual(tokens))
+})
+
+
+test.skip('(add 1 2)', () => {
+  const code = `(add 1 2)`
+  const tokens = [
+    {type: TokenTypes.Paren, value: "("},
+    {type: TokenTypes.Name, value: "add"},
+    {type: TokenTypes.Number, value: "1"},
+    {type: TokenTypes.Number, value: "2"},
+    {type: TokenTypes.Paren, value: ")"},
+  ];
+
+  expect(tokenizer(code).toEqual(tokens))
+})
---
+++ tokenizer.ts
@@ -0,0 +1,81 @@
+export enum TokenTypes {
+  Paren,
+  Name,
+  Number,
+}
+
+interface Token {
+  type: TokenTypes
+  value: string
+}
+
+
+export function tokenizer(code: string) {
+  const tokens: Token[] = []
+  let current = 0
+
+  while (current < code.length) {
+    let char = code[current]
+
+    // 匹配是空格
+    const WHITESSPACE = /\s/;
+    if(WHITESSPACE.test(char)) {
+      current ++;
+      continue;
+    }
+
+
+    // 匹配是 (
+    if (char === '(') {
+      tokens.push({
+        type: TokenTypes.Paren,
+        value: char,
+      })
+      current ++;
+      continue;
+    }
+
+    if (char === ') ') {
+      tokens.push({
+        type: TokenTypes.Paren,
+        value: char,
+      })
+       current++
+       continue
+    }
+
+    // 匹配是字母
+    const LETTERS = /[a-z]/i
+    if (LETTERS.test(char)) {
+      let value = ''
+      while (LETTERS.test(char) && current < code.length) {
+        // 下一个字符还是字母的话
+        value += char
+        char = code[++current]
+        console.log(char, current)
+      }
+      tokens.push({
+        type: TokenTypes.Name,
+        value,
+      })
+    }
+
+    // 匹配是数字
+    const Number = /[a-z]/i
+    if (Number.test(char)) {
+      let value = ''
+      while (Number.test(char) && current < code.length) {
+        // 下一个字符还是字母的话
+        value += char
+        char = code[++current]
+        console.log(char, current)
+      }
+      tokens.push({
+        type: TokenTypes.Number,
+        value,
+      })
+    }
+  }
+
+  return tokens
+}
